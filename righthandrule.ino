eb092f8341723abc1b03f77d5a73c4039190bf3c
bd6021197-cpu <bd6021197@ahschool.com>
2025-12-15 15:42:23 -0500

    Update righthandrule.ino

--- righthandrule.ino
+++ righthandrule.ino
@@ -5,6 +5,7 @@ MeUltrasonicSensor ultraSonic(PORT_2);
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 float turned_right = false;
+float go_forward = true;
 
 void forward(){
   motor1.run(-100);
@@ -12,16 +13,24 @@ void forward(){
 }
 
 void turn_right(){
-  motor1.run(100);
-  motor1.run(100);
-  delay(560);
+  motor1.stop();
+  motor2.stop();
+  go_forward = false;
+  motor1.run(-100);
+  motor2.run(-100);
+  delay(555);
+  go_forward = true;
 }
 
 void turn_180(){
-  motor1.run(100);
-  motor1.run(100);
-  delay(1120);
+  motor1.stop();
+  motor2.stop();
+  go_forward = false;
+  motor1.run(-100);
+  motor2.run(-100);
+  delay(1110);
   turned_right = false;
+  go_forward = true;
 }
 
 void setup() {
@@ -32,8 +41,10 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(ultraSonic.distanceCm());
   Serial.println(" cm");
-  if (ultraSonic.distanceCm() > 10){
-    forward();
+  if (ultraSonic.distanceCm() >  10){
+    if (go_forward){
+      forward();
+    }
   } else {
     if (turned_right){
       turn_180();
