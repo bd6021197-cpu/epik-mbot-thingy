51b8e8ffc36c8a7967c8c69f7e47f781a781b0fd
bd6021197-cpu <bd6021197@ahschool.com>
2025-12-16 15:34:21 -0500

    Update righthandrule.ino

--- righthandrule.ino
+++ righthandrule.ino
@@ -5,7 +5,6 @@ MeUltrasonicSensor ultraSonic(PORT_2);
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 float turned_right = false;
-float go_forward = true;
 
 void forward(){
   motor1.run(-100);
@@ -15,22 +14,17 @@ void forward(){
 void turn_right(){
   motor1.stop();
   motor2.stop();
-  go_forward = false;
-  motor1.run(-100);
-  motor2.run(-100);
-  delay(555);
-  go_forward = true;
+  motor1.run(105);
+  motor2.run(105);
+  delay(550);
 }
 
-void turn_180(){
+void turn_left(){
   motor1.stop();
   motor2.stop();
-  go_forward = false;
-  motor1.run(-100);
-  motor2.run(-100);
-  delay(1110);
-  turned_right = false;
-  go_forward = true;
+  motor1.run(-105); 
+  motor2.run(-105);
+  delay(550);
 }
 
 void setup() {
@@ -41,16 +35,18 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(ultraSonic.distanceCm());
   Serial.println(" cm");
-  if (ultraSonic.distanceCm() >  10){
-    if (go_forward){
-      forward();
-    }
+  if (ultraSonic.distanceCm() >  8){
+    forward(); //go forward until there is a wall
   } else {
-    if (turned_right){
-      turn_180();
+    if (turned_right == false){
+      turn_right(); //if the mBot hasn't tried turning right yet, turn right
+      if (ultraSonic.distanceCm() <=  8){
+        turned_right = true;
+        turn_left(); //if the mBot tried turning right and there is still a wall infront of it do a left turn
+      }
     } else {
-      turn_right();
-      turned_right = true;
+      turn_left(); // try the left path
+      turned_right = false; 
     }
   }
 }
